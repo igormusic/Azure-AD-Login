c9fdec47b646ae74e7bcab99fd70c43c04f3f3fd
Igor Music <igormusich@gmail.com>
2022-09-10 15:59:51 -0400

    first commit

---
+++ AzureAdLogin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+namespace TVMAzureAd
+
+{
+
+    public interface IAzureLogin
+    {
+
+        String Login(string ClientId, string Tenant, string Instance);
+    }
+
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class AzureLogin:IAzureLogin
+    {
+        string[] scopes = new string[] { "user.read" };
+
+        public AzureLogin() {
+ 
+        }
+
+
+        [ComVisible(true)]
+        public String Login(string ClientId, string Tenant, string Instance) {
+
+            String result = "failed";
+
+            try
+            {
+                var builder = PublicClientApplicationBuilder.Create(ClientId)
+                   .WithAuthority($"{Instance}{Tenant}")
+                   .WithDefaultRedirectUri();
+
+                var app = builder.Build();
+
+
+                var task = app.AcquireTokenInteractive(scopes)
+                           .WithPrompt(Prompt.SelectAccount)
+                           .ExecuteAsync();
+
+                var taskResult = task.GetAwaiter().GetResult();
+
+                result = taskResult.AccessToken;
+
+            }
+            catch (Exception ex) {
+                result = "ex:" + ex.Message;
+            }
+            return result;
+
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+﻿# Azure AD Login
+
+Expose Azure AD Login as COM DLL accessible from VB 6 and VBA
+
+Example:
+```vb 6
+
+ Dim login As New TVMAzureAd.AzureLogin
+ Dim token as String
+ token = login.login("974e60ee-7add-4f7e-a3b7-1f9dfd4e32b7", "common", "https://login.microsoftonline.com/")
+    
+
+```
