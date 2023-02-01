90fc510dbfabe9c6247b7829ee353586b8aec59d
Igor Music <igormusich@gmail.com>
2023-02-01 17:44:02 -0500

    dev

--- AzureAdLogin.cs
+++ AzureAdLogin.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Graph;
 using Microsoft.Identity.Client;
 namespace TVMAzureAd
 
@@ -19,13 +21,16 @@ namespace TVMAzureAd
         string ErrorMessage { get; }
 
         string UserName { get; }
+
+        string[] Groups { get; }
     }
 
     [ComVisible(true)]
     [ClassInterface(ClassInterfaceType.None)]
     public class AzureLogin:IAzureLogin
     {
-        string[] scopes = new string[] { "user.read" };
+        readonly string[] _scopes = new string[] { "user.read" };
+        string[] _groups;
         private string _accessToken;
         private string _errorMessage;
         private string _userName;
@@ -41,8 +46,10 @@ namespace TVMAzureAd
         [ComVisible(true)]
         public string UserName => _userName;
 
+        public string[] Groups => _groups;
+
         [ComVisible(true)]
-        bool IAzureLogin.Login(string ClientId, string Tenant, string Instance)
+        public bool Login(string ClientId, string Tenant, string Instance)
         {
 
             bool result;
@@ -55,30 +62,59 @@ namespace TVMAzureAd
                 var app = builder.Build();
 
 
-                var task = app.AcquireTokenInteractive(scopes)
-                           .WithPrompt(Prompt.SelectAccount)
+                var loginTask = app.AcquireTokenInteractive(_scopes)
+                           .WithPrompt(Microsoft.Identity.Client.Prompt.SelectAccount)
                            .ExecuteAsync();
 
-                var taskResult = task.GetAwaiter().GetResult();
+                var loginResult = loginTask.GetAwaiter().GetResult();
 
-                _accessToken = taskResult.AccessToken;
+                _accessToken = loginResult.AccessToken;
 
-                if (taskResult.Account != null)
+                if (loginResult.Account != null)
                 {
-                    _userName = taskResult.Account.Username;
+                    _userName = loginResult.Account.Username;
 
                 }
 
+                GetGroups();
+
+                //_groups = new string[] { "group-A", "group-B" } ;
+                    
                 result = true;
             }
             catch (Exception ex)
             {
-                _errorMessage = ex.Message;
+                _errorMessage = ex.ToString();
                 result = false;
 
             }
 
             return result;
+        }
+
+        private void GetGroups()
+        {
+            var graphServiceClient = new GraphServiceClient(new DelegateAuthenticationProvider((requestMessage) =>
+            {
+                requestMessage
+                    .Headers
+                    .Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
+
+                return Task.CompletedTask;
+            }));
+
+            var groupTask = graphServiceClient.Me.MemberOf.Request().GetAsync();
+
+            var groupResult = groupTask.GetAwaiter().GetResult();
+
+            var groupNames = new List<string>();
+
+            foreach (Group group in groupResult)
+            {
+                groupNames.Add(group.DisplayName);
+            }
+
+            _groups = groupNames.ToArray();
         }
     }
 }
